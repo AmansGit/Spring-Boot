c395b308d5bbddb8948d99823b942369d901baec
AmansGit <amans1997a@gmail.com>
2020-08-21 17:20:32 +0530

    Revert "Interface added"
    This reverts commit 6c4806a7

--- workspace-new-one/bootjpa/src/main/java/com/database/bootjpa/dao/AmanRepo.java
+++
@@ -1,7 +0,0 @@
-package com.database.bootjpa.dao;
-
-import com.database.bootjpa.model.AmanBrand;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AmanRepo extends CrudRepository<AmanBrand, Integer> {
-}
